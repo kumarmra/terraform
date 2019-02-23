4a578faae22774ccd5294b025d66d698f051128d
kumarmra <46415848+kumarmra@users.noreply.github.com>
2019-02-23 19:20:23 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,8 @@
+resource "aws_security_group" "web" {
+  name        = "web-access"
+  description = "Security group to allow access from mgmt vpc"
+  vpc_id      = "vpc-0eca6d74"
+   tags {
+    Name = "web-access"
+  }
+ }
